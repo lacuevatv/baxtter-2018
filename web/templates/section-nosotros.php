963368dafb481971652bd913b38bdcfdca8e0dbb
coco <cocodelacueva@gmail.com>
2018-06-12 15:52:47 -0300

    le agregué el video por vimeo

--- web/templates/section-nosotros.php
+++ web/templates/section-nosotros.php
@@ -106,9 +106,19 @@ global $dispositivo;
                     <span class="sr-only">Video Baxtter</span>
                 </h3>
 
-                <!-- TEMP IMAGE -->
-                <img src="<?php echo UPLOADSURL; ?>/ref-video.jpg" style="width:100%;display: block; margin: 0 auto;">
-            
+                
+                <!--<img src="<?php echo UPLOADSURL; ?>/ref-video.jpg" style="width:100%;display: block; margin: 0 auto;">-->
+                <!-- VIDEO -->
+                <div style="padding:42.19% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/272949856?autoplay=1&loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                    <script>
+                    var iframe = document.querySelector('iframe');
+                    var options = {
+                        loop: true
+                    };
+                    var player = new Vimeo.Player(iframe,options);
+                    player.setVolume(0);
+                    player.play(); 
+                    </script>
             </div>
         </div>
     </div>
