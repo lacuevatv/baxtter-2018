3ecb50915f4ad2b61a3c68d15e193fa2f433736a
coco <coco@lacueva.tv>
2019-04-30 19:25:26 -0300

    instagram y otras secciones

--- web/templates/section-instagram.php
+++ web/templates/section-instagram.php
@@ -1,13 +1,15 @@
 <section id="presentaciones-online" class="section-capas instagram-section">
-    
-    <div class="main-section container">
 
-        <div class="main-section-wrapper">
+    <div class="main-section container">
             
-            <h2>
-                Instagram
-            </h2>
+        <h2 class="titulo-seccion titulo-section-instagram">
+            @SomosBaxtter
+        </h2>
+
+        <div class="instagram-feed-wrapper">
+            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="http://lightwidget.com/widgets/d6729a3a3bb65a269021b72e89e32d6c.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
         </div>
+        
     </div><!-- //. container -->
 
     <!-- front -->
