301a2b810ffaf8fc308ad3edf3a88add9f8325b8
coco <coco@lacueva.tv>
2019-05-13 14:06:31 -0300

    reordene los campos del email de reunion

--- public/templates/formularios.php
+++ public/templates/formularios.php
@@ -21,7 +21,7 @@ switch ($data) {
 			<div class="inner-wrapper-form margin-right">
 
 				<div class="form-group">
-					<input type="text" name="name-school">
+					<input type="text" name="name-school" required>
 					<label for="name-school">Nombre del colegio</label>
 					<span class="msj-error-input">
 						Este campo nos ayuda a comunicarnos
@@ -29,7 +29,16 @@ switch ($data) {
 				</div>
 
 				<div class="form-group">
-					<input type="number" name="alumnos">
+					<input type="text" name="localidad" required>
+					<label for="localidad">Localidad</label>
+					<span class="icon-input"></span>
+					<span class="msj-error-input">
+						La localidad es indispensable
+					</span>
+				</div>
+
+				<div class="form-group">
+					<input type="number" name="alumnos" required>
 					<label for="alumnos">Cantidad de alumnos</label>
 					<span class="msj-error-input">
 						Debería escribir sólo números, no utilice separadores
@@ -37,7 +46,28 @@ switch ($data) {
 				</div>
 
 				<div class="form-group">
-					<input type="text" name="charge">
+					<input type="number" name="year-trip" required>
+					<label for="year-trip">Año de viaje</label>
+					<span class="msj-error-input">
+						Debería escribir sólo números, no utilice separadores
+					</span>
+				</div>
+
+			</div><!-- //.inner-wrapper-form -->
+
+			<div class="inner-wrapper-form margin-left">
+
+				<div class="form-group">
+					<input type="text" name="name" class="text-uppercase" required>
+					<label for="name" class="text-uppercase">Tu nombre y apellido</label>
+					<span class="icon-input"></span>
+					<span class="msj-error-input">
+						Este campo nos resulta de mucha utilidad
+					</span>
+				</div>
+
+				<div class="form-group">
+					<input type="text" name="charge" required>
 					<label for="charge">Sos Alumno / padre / madre / otro</label>
 					<span class="icon-input"></span>
 					<span class="msj-error-input">
@@ -53,42 +83,12 @@ switch ($data) {
 					</span>
 				</div>
 
-			</div><!-- //.inner-wrapper-form -->
-
-			<div class="inner-wrapper-form margin-left">
-
 				<div class="form-group">
-					<input type="number" name="year-trip">
-					<label for="year-trip">Año de viaje</label>
-					<span class="msj-error-input">
-						Debería escribir sólo números, no utilice separadores
-					</span>
-				</div>
-
-				<div class="form-group">
-					<input type="text" name="name" class="text-uppercase">
-					<label for="name" class="text-uppercase">Tu nombre y apellido</label>
-					<span class="icon-input"></span>
-					<span class="msj-error-input">
-						Este campo nos resulta de mucha utilidad
-					</span>
-				</div>
-
-				<div class="form-group">
-					<input type="number" name="tel">
+					<input type="number" name="tel" required>
 					<label for="tel">Teléfono Movil</label>
 					<span class="icon-input"></span>
 					<span class="msj-error-input">
 						El teléfono es necesario para comunicarnos (no use separadores)
-					</span>
-				</div>
-
-				<div class="form-group">
-					<input type="text" name="localidad">
-					<label for="localidad">Localidad</label>
-					<span class="icon-input"></span>
-					<span class="msj-error-input">
-						La localidad es indispensable
 					</span>
 				</div>
 				
